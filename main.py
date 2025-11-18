2ce69d47cd40d5d0f93cac78c0a7ce98e269a72f
Evelina Sedrakyan <evelina.sedrakyan@student.rau.am>
2025-11-18 18:01:58 +0400

    Add main loop

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import time
 import random
 from analyzer import Analyzer
-
 config_path = r"C:\Users\Acer\Desktop\simple_analyzer\config\config.txt"
 
-interval = 0
-sequence_length = 0
+interval = 2
+sequence_length = 18
 
 with open(config_path, "r") as f:
     for line in f:
@@ -19,18 +18,21 @@ print("Sequence length:", sequence_length)
 
 analyzer = Analyzer()
 
-while True:
-    num = random.randint(1, 100)
-    analyzer.add_number(num)
-    if len(analyzer.numbers) > sequence_length:
-        analyzer.numbers.pop(0)
-    print("Numbers:", analyzer.numbers)
-    print("Even:", analyzer.even_count())
-    print("Odd:", analyzer.odd_count())
-    print("Highest:", analyzer.highest_number())
-    print("Increasing pairs:", analyzer.increasing_pairs())
+try:
+    while True:
+        num = random.randint(1, 100)
+        analyzer.add_number(num)
+        if len(analyzer.numbers) > sequence_length:
+            analyzer.numbers.pop(0)
+        print("Numbers:", analyzer.numbers)
+        print("Even:", analyzer.even_count())
+        print("Odd:", analyzer.odd_count())
+        print("Highest:", analyzer.highest_number())
+        print("Increasing pairs:", analyzer.increasing_pairs())
+        if len(analyzer.numbers) == sequence_length and time.localtime().tm_sec == 0:
+            print("Stopping program.")
+            break
+        time.sleep(interval)
 
-    if len(analyzer.numbers) == sequence_length and time.localtime().tm_sec == 0:
-        print("Stopping program.")
-        break
-    time.sleep(interval)
+except KeyboardInterrupt:
+    print("\nStopping program")
