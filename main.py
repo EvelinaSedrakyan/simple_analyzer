256eaa2a6524935bc2562caf799982bc3b3c6f10
Evelina Sedrakyan <evelina.sedrakyan@student.rau.am>
2025-11-18 17:30:05 +0400

    Add main loop

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import time
+import random
+from analyzer import Analyzer
+
 config_path = r"C:\Users\Acer\Desktop\simple_analyzer\config\config.txt"
 
 interval = 0
@@ -12,3 +16,21 @@ with open(config_path, "r") as f:
 
 print("Interval:", interval)
 print("Sequence length:", sequence_length)
+
+analyzer = Analyzer()
+
+while True:
+    num = random.randint(1, 100)
+    analyzer.add_number(num)
+    if len(analyzer.numbers) > sequence_length:
+        analyzer.numbers.pop(0)
+    print("Numbers:", analyzer.numbers)
+    print("Even:", analyzer.even_count())
+    print("Odd:", analyzer.odd_count())
+    print("Highest:", analyzer.highest_number())
+    print("Increasing pairs:", analyzer.increasing_pairs())
+
+    if len(analyzer.numbers) == sequence_length and time.localtime().tm_sec == 0:
+        print("Stopping program.")
+        break
+    time.sleep(interval)
